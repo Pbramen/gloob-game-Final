9f30ebdecf96d6a5124ed9d2957d836f8f04d0e1
Peter Banh <peterbanhe.e@gmail.com>
2023-04-30 14:26:29 -0500

    Two Rooms
    + one shop room

--- Gloob/Assets/Scripts/monitorEnemies.cs
+++ Gloob/Assets/Scripts/monitorEnemies.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
+using System.Linq;
 public class monitorEnemies : MonoBehaviour
 {
     public List<GameObject> enemies;
-    public int count;
+    public int count =0;
     public UnityEvent openDoor;
     public GenRoom thisRoom;
+    bool nonCombat = false;
     void Start()
     {
         thisRoom = GetComponentInParent<GenRoom>();
@@ -17,9 +18,16 @@ public class monitorEnemies : MonoBehaviour
             enemies.Add(i.gameObject);
         }
         count = transform.childCount;
-        //set all to inactive(should maybe do this pre-compile time)
-        foreach (GameObject i in enemies) {
-            i.SetActive(false);
+        if (count == 0) {
+            nonCombat = true;
+        }
+        else
+        {
+            //set all to inactive(should maybe do this pre-compile time)
+            foreach (GameObject i in enemies)
+            {
+                i.SetActive(false);
+            }
         }
 
         StartCoroutine(generateEnemies());
@@ -27,9 +35,18 @@ public class monitorEnemies : MonoBehaviour
 
     IEnumerator generateEnemies(){
         yield return new WaitUntil(() => ModularLevelGenerator.scanComplete && thisRoom.active);
-        foreach(GameObject i in enemies){
-            i.SetActive(true);
-            yield return null;
+        if (nonCombat) {
+            Debug.Log("door is opened!");
+            yield return new WaitForSeconds(1f);
+            openDoor?.Invoke();
+        }
+        else
+        {
+            foreach (GameObject i in enemies)
+            {
+                i.SetActive(true);
+                yield return null;
+            }
         }
     }
 
